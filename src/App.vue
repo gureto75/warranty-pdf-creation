ebcca267bb3739cda2aa35032c6434ea993b5a02
gureto <gureto75@gmail.com>
2021-05-26 16:27:15 +0300

    Halfway there

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,105 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="form" v-on:keyup.enter="handleClick">
+    <label for="client">Ονοματεπώνυμο</label>
+    <input type="text" ref="client" id="client" v-model="client" />
+    <label for="brand">Εταιρεία</label>
+    <input type="text" ref="brand" id="brand" v-model="brand" />
+    <label for="model">Μοντέλο</label>
+    <input type="text" ref="model" id="model" v-model="model" />
+    <label for="serial">Χρώμα</label>
+    <input type="text" ref="color" id="color" v-model="color" />
+    <label for="serial">Σειριακός</label>
+    <input type="text" ref="serial" id="serial" v-model="serial" />
+    <button @click="handleClick">Show Modal</button>
+  </div>
+  <div v-if="showModal">
+    <Modal
+      :modalTitle="modalHeader"
+      :modalContent="modalContent"
+      :brand="brand"
+      :model="model"
+      :color="color"
+      :serial="serial"
+      :clientName="client"
+      theme=""
+      @close="handleClick"
+    >
+      <template v-slot:links>
+        <a href="">Sign Up Now</a>
+        <a href="">More Info</a>
+      </template>
+      <!-- <h1>Slot Title</h1>
+      <p>Slot Content</p> -->
+    </Modal>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import Modal from './components/Modal.vue'
+  export default {
+    components: { Modal },
+    name: 'App',
+    data() {
+      return {
+        modalContent: 'Πατα το κουμπί για να προχωρήσεις',
+        brand: '',
+        model: '',
+        serial: '',
+        color: '',
+        client: '',
+        showModal: false,
+      }
+    },
+    computed: {
+      modalHeader() {
+        return `${this.brand} ${this.model} ${this.color}`
+      },
+    },
+    methods: {
+      handleClick() {
+        // console.dir(this.$refs.name.value)
+        // this.$refs.name.classList.add('active')
+        this.showModal = !this.showModal
+      },
+    },
+    mounted() {
+      this.$refs.client.focus()
+    },
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .form button {
+    margin-top: 20px;
+    padding: 16px 32px;
+    background: rgb(7, 202, 202);
+    border-radius: 5px;
+  }
+
+  input {
+    display: block;
+    padding: 16px 10px;
+    border: 1px solid #555;
+    border-radius: 5px;
+  }
+
+  label {
+    display: inline-block;
+    margin-top: 20px;
+  }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,214 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+      <h1>{{ modalTitle }}</h1>
+      <p>{{ modalContent }}</p>
+      <slot>...</slot>
+      <div>
+        <button class="download" @click="createPdfMake">Save PDF</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import pdfMake from 'pdfmake/build/pdfmake'
+  import pdfFonts from '../assets/vfs_fonts.js'
+  pdfMake.vfs = pdfFonts.pdfMake.vfs
+  export default {
+    name: 'Modal',
+    props: [
+      'model',
+      'brand',
+      'color',
+      'serial',
+      'clientName',
+      'modalTitle',
+      'modalContent',
+      'theme',
+    ],
+    computed: {
+      pdfName() {
+        return `${this.clientName}_Warranty`
+      },
+    },
+    methods: {
+      closeModal() {
+        this.$emit('close')
+      },
+      createPdfMake() {
+        const date = new Date()
+        const expiration = new Date()
+        expiration.setDate(date.getDate() + 183)
+        const buyDate = new Intl.DateTimeFormat('el-GR', {
+          dateStyle: 'short',
+        }).format(date)
+        const expDate = new Intl.DateTimeFormat('el-GR', {
+          dateStyle: 'short',
+        }).format(expiration)
+        const dd = {
+          info: {
+            title: `${this.pdfName}_ends_in_${expDate}`,
+            author: 'atmosfaira',
+            subject: 'Warranty',
+            keywords: 'Warranty',
+          },
+          styles: {
+            header: {
+              fontSize: 18,
+              bold: true,
+              decoration: 'underline',
+            },
+            tableRegular: {
+              margin: [0, 20, 0, 0],
+            },
+            tableHeader: {
+              bold: true,
+              fontSize: 13,
+              color: 'black',
+            },
+          },
+          content: [
+            {
+              image: 'atmosfaira-logo.jpg',
+              width: 150,
+            },
+            {
+              text: 'ΕΓΓΥΗΣΗ ΚΑΛΗΣ ΛΕΙΤΟΥΡΓΙΑΣ ΣΥΣΚΕΥΗΣ',
+              margin: [0, 10],
+              style: 'header',
+              alignment: 'center',
+            },
+            {
+              columns: [
+                { width: '*', text: '' },
+                {
+                  width: 'auto',
+                  style: 'tableRegular',
+                  table: {
+                    headerRows: 1,
+                    // dontBreakRows: true,
+                    // keepWithHeaderRows: 1,
+                    body: [
+                      [
+                        { text: 'ΣΥΣΚΕΥΗ', style: 'tableHeader' },
+                        { text: 'ΧΡΩΜΑ', style: 'tableHeader' },
+                        { text: '#Serial', style: 'tableHeader' },
+                      ],
+                      [`${this.brand} ${this.model}`, this.color, this.serial],
+                    ],
+                  },
+                },
+                ,
+                { width: 'auto', text: '' },
+              ],
+            },
+            {
+              columns: [
+                { width: '*', text: '' },
+                {
+                  width: 'auto',
+                  style: 'tableRegular',
+                  table: {
+                    headerRows: 1,
+                    // dontBreakRows: true,
+                    // keepWithHeaderRows: 1,
+                    body: [
+                      [
+                        { text: 'ΗΜ/ΝΙΑ ΑΓΟΡΑΣ', style: 'tableHeader' },
+                        {
+                          text: 'ΙΣΧΥΣ ΕΓΓΥΗΣΗΣ ΕΩΣ',
+                          style: 'tableHeader',
+                        },
+                      ],
+                      [
+                        { text: buyDate, alignment: 'center' },
+                        { text: expDate, alignment: 'center' },
+                      ],
+                    ],
+                  },
+                },
+                ,
+                { width: 'auto', text: '' },
+              ],
+            },
+            {
+              image: 'signature.png',
+              width: 240,
+              alignment: 'right',
+              margin: [0, 20, 15, 0],
+            },
+          ],
+          defaultStyle: { font: 'Jura' },
+        }
+
+        pdfMake.fonts = {
+          Jura: {
+            normal: 'Jura-Regular.ttf',
+            bold: 'Jura-Bold.ttf',
+          },
+        }
+
+        pdfMake.createPdf(dd).download(`${this.pdfName}_ends_in_${expDate}`)
+        pdfMake.createPdf(dd).open()
+        // pdfMake.createPdf(dd).print()
+      },
+    },
+  }
+</script>
+
+<style>
+  .modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+  }
+
+  .backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+  }
+  .modal h1 {
+    color: #03cfb4;
+  }
+
+  .modal .actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+  }
+
+  .modal .actions a,
+  .download {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+  }
+
+  .modal.sale {
+    background: crimson;
+    color: white;
+  }
+
+  .modal.sale h1 {
+    color: white;
+  }
+
+  .modal.sale .actions {
+    color: white;
+  }
+
+  .modal.sale .actions a {
+    color: white;
+  }
+</style>
